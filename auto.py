df6e932dc1e166a9ab3ae61a51f977bd02827b93
Santosh kurmi <xantosh121@gmail.com>
2022-06-12 17:50:59 +0545

    first comit

--- auto.py
+++ auto.py
@@ -1,1 +1,13 @@
-print("Hello")
+#version 1.0
+import os
+
+repo="https://github.com/Santoshkurmi/python_auto_update"
+if os.path.exists(".temp"):
+    os.system("rm .temp -rf");
+
+if not os.system(f"git clone {repo} .temp"):
+    file = open(".temp/.version_python");
+    version=float(file.read());
+    print(version);
+    
+  
