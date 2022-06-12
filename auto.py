b0536cbd52a122f588d27a9cf215d3859475925b
Santosh kurmi <xantosh121@gmail.com>
2022-06-12 18:26:12 +0545

    first comita

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 #version 1.1
 #latest update of 12 jun 6:20 pm
 import os
-from datetime import datetime
+from time
 
 
 repo="https://github.com/Santoshkurmi/python_auto_update"
@@ -20,7 +20,7 @@ def check_update(repo,tempdir,filename):
 
     if current>=1 and latest>=1:
         if current<latest:
-            time=datetime.now();
+            time=time.time();
             print("Update is available,updating");
             if os.system(f"cp {filename}.py .{filename}_{time}"):
                 if os.system(f"cp {tempdir}/{filename}.py ."):
