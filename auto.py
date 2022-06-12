451199f7c3cb0aff958cc69e7ae7a86ff0f2ce51
Santosh kurmi <xantosh121@gmail.com>
2022-06-12 17:55:11 +0545

    first comita

--- auto.py
+++ auto.py
@@ -5,7 +5,7 @@ repo="https://github.com/Santoshkurmi/python_auto_update"
 if os.path.exists(".temp"):
     os.system("rm .temp -rf");
 
-if not os.system(f"git clone {repo} .temp"):
+if not os.system(f"git clone {repo} .temp && clear"):
     file = open(".temp/auto.py");
     version=file.readline().replace("#version","");
     print(version);
