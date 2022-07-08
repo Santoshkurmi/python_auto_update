a43d5530fb870f11a4a98e442ed0cc528122465b
Santosh kurmi <xantosh121@gmail.com>
2022-07-08 17:37:28 +0545

    f8 july

--- ncellpy.py
+++ ncellpy.py
@@ -1082,7 +1082,7 @@ def login():
                             }
                             }
                             response=post("user/otp/validate",body);
-                            print(response[1])
+                            dialog_text("-m",response[1],"Upps try again later")
                             if response[0]==200:
                                 try:
                                     current=response[2]["validateOTPResponse"]["accessToken"]
