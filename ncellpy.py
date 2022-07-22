a3cc1bbc0bd61b78b276cc29868fdbefdec191d0
Santosh kurmi <xantosh121@gmail.com>
2022-07-22 17:51:21 +0545

    20 july

--- ncellpy.py
+++ ncellpy.py
@@ -117,10 +117,10 @@ def update(repo,filename,tempdir=".temp",dust=0):
                         set_update_time()
                         # print(f"\n{c()}Please restart the program")
                         # Popen("python3 ncellpy.py",shell=True)
-                        if back_thread==0:
-                            # os.system("clear && python3 ncellpy.py")
-                            exit();
-                        else:exit()
+                        # if back_thread==0:
+                        os.system("clear && python3 ncellpy.py")
+                        exit();
+                        # else:exit()
                         
                 if os.path.exists(tempdir):
                     os.system(f"rm {tempdir} -rf");
@@ -143,10 +143,10 @@ def update(repo,filename,tempdir=".temp",dust=0):
                         print(f"\n{c()}_________________________")
                         set_update_time()
                         # Popen("python3 ncellpy.py",shell=True)
-                        if back_thread==0:
-                            # os.system("clear && python3 ncellpy.py")
-                            exit();
-                        else:exit()
+                        # if back_thread==0:
+                        os.system("clear && python3 ncellpy.py")
+                        exit();
+                        # else:exit()
                 if os.path.exists(tempdir):
                     os.system(f"rm {tempdir} -rf");
 
@@ -177,12 +177,12 @@ def set_update_time():
 
 
 
-back_thread=0
+# back_thread=0
 if func_wow==0 and check_update_time():
-    # update(repo,"ncellpy");
+    update(repo,"ncellpy");
     # print("Hello baby")
-    back_thread=1
-    threading.Thread(target=update,args=(repo,"ncellpy",)).start()
+    # back_thread=1
+    # threading.Thread(target=update,args=(repo,"ncellpy",)).start()
 
 
 ###############################update code above
@@ -2635,9 +2635,9 @@ while(True):
     #     if code==-1:
     #         choose=index+1
     # else:
-    if back_thread==1:
-        os.system("clear && python3 ncellpy.py")
-        exit();
+    # if back_thread==1:
+    #     os.system("clear && python3 ncellpy.py")
+    #     exit();
     os.system("clear");print(f"{c()}We{c()}lc{c()}om{c()}e {c()}to {c()}nc{c()}el{c()}l m{c()}od{c()}s{c()}\n__{c()}_______{c()}________{c()}____")
     
     choose=take(f"\n{c()}1.{c()} Ncell {c()}Ecare\n{c()}2.{c()} Ncell {c()}App{c()}\n3. {c()}Ncell{c()} Web\n{c()}4. {c()}MoviesVerse\n{c()}5.{c()} Update{c()}\n=>");
