0c8bb8cd92d6cec3a0137514b8368d74e7f322f3
Santosh kurmi <xantosh121@gmail.com>
2022-07-22 16:51:04 +0545

    20 july

--- ncellpy.py
+++ ncellpy.py
@@ -644,6 +644,7 @@ def checkData(operation=0):
                 i+=1;
                 idd.append([f["ACCT_RES_ID"],f["UNIT_NAME"] ])
     print("****************************\n")
+    if operation==0:input()
     return idd;
 
 def signup():
@@ -1937,8 +1938,8 @@ def web():
     while(True):
         # print("I am here")
         # if not choice in [1]:
-        #     os.system("clear");print(f"{c()}We{c()}lc{c()}om{c()}e {c()}to {c()}nc{c()}el{c()}l m{c()}od{c()}s{c()}\n__{c()}_______{c()}________{c()}____") 
-        # # print("\n***********************************\n")
+        os.system("clear");print(f"{c()}We{c()}lc{c()}om{c()}e {c()}to {c()}nc{c()}el{c()}l m{c()}od{c()}s{c()}\n__{c()}_______{c()}________{c()}____") 
+        # print("\n***********************************\n")
         for i in range(len(fun_list)):
             print(f'{c()}{i+1}. {fun_list[i][0]}');
         
